8a270c306ee7c212a8572ffe59791dab6234251f
Sami Ur Rahman <sami.khan244409github@gmail.com>
2024-12-20 22:27:25 +0500

    first commit

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const fs = require('fs');
+const path = require('path');
+const app = express();
+
+// Define the path to the JSON file
+const dataFilePath = path.join(__dirname, 'data.json');
+
+// Define a route
+app.get('/', (req, res) => {
+    // Read the JSON file and send the data as the response
+    fs.readFile(dataFilePath, 'utf8', (err, data) => {
+        if (err) {
+            return res.status(500).json({ error: 'Failed to read data' });
+        }
+        res.json(JSON.parse(data));
+    });
+});
+
+// Start the server
+const PORT = 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
